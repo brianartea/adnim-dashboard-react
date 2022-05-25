1f0cec5482f35670ec7ea5bbb28c471501e0ca6e
brianartea <bri@expertdesign.cc>
2022-05-24 21:55:31 -0400

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Build and Deploy a React Admin Dashboard App With Theming, Tables, Charts, Calendar, Kanban and More
+![Shoppy](https://i.ibb.co/W6g39w3/image.png)
+
+## Introduction
+Make sure to use single quotes instead of double quotes or it will not do a production build.
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24 mb-7">
+    <p className="dark:text-gray-200 text-gray-700 text-center">
+      © {new Date().getFullYear()} All rights reserved. Shoppy.com
+    </p>
+  </div>
+);
+
+export default Footer;
